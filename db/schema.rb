6e52e68b849f7d58383482374fd5401987dfb0a3
Richard Branson <rmbranson@gmail.com>
2014-11-26 13:26:14 +0000

    Added Image Column to db Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125210707) do
+ActiveRecord::Schema.define(version: 20141126132415) do
 
   create_table "movies", force: true do |t|
     t.text     "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141125210707) do
     t.integer  "year_release"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
